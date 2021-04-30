ca85c13b0eea63e04e1715ff8dc23759a82627b7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-30 11:43:14 +0300

    Initial Commit

---
+++ AppEnvironmentDependencyInjection/AppEnvironmentDependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEnvironmentDependencyInjectionApp.swift
+//  AppEnvironmentDependencyInjection
+//
+//  Created by ramil on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppEnvironmentDependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppEnvironmentDependencyInjection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppEnvironmentDependencyInjection
+//
+//  Created by ramil on 30.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
